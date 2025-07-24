eff8c8b84f9f6b8580841643c09e73f80568481b
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-24 09:12:42 -0300

    Criando o Form do projeto

--- costs/src/components/pages/NewProjects.js
+++ costs/src/components/pages/NewProjects.js
@@ -1,7 +1,12 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProjects.module.css'
+
 function NewProjects(){
     return(
-        <div>
-            <h1>NewProjects</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
         </div>
     )
 }
