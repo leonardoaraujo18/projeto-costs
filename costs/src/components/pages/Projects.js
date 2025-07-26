e30cc27a1308532befbbd1292618c6e41c26524d
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-26 08:37:57 -0300

    Projeto React

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,7 +1,15 @@
+import styles from './Projects.module.css'
+
 import {useLocation} from 'react-router-dom'
 import Message from "../layout/Message";
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
+import { useState, useEffect } from 'react';
 
 function Projects(){
+
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
@@ -9,10 +17,38 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/project', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message &&  <Message type="success" msg={message}  />}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard name={project.name} 
+                        id={project.id} 
+                        budget={project.budget} 
+                        category={project.category?.name || 'Categoria Desconhecida'} 
+                        key={project.id} 
+                         />
+                    ))}
+            </Container>
         </div>
     )
 }
